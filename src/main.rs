f8bd13108e2a102fe45a4c317e3a56436264e2f7
crisnet-dev <cristianobonguepedro02@gmail.com>
2026-09-03 10:46:42 +0100

    Using impl to organize the code

--- src/main.rs
+++ src/main.rs
@@ -3,94 +3,106 @@
 
 use core::panic::PanicInfo;
 
-const VGA_WIDTH: usize = 80;
-const VGA_HEIGHT: usize = 25;
-const VGA_ADDRESS: usize = 0xb8000;
-
-const WHITE: u8 = 0x0F;
-const BLUE: u8 = 0x9;
-
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop {
-        core::hint::spin_loop();
-    }
+    loop {}
 }
 
-fn clear_screen(color: u8) {
-    let vga = VGA_ADDRESS as *mut u8;
+#[repr(u8)]
+enum Color {
+    BLUE = 0x9,
+    //GREEN = 0x2,
+    WHITE = 0x0F,
+}
 
-    for i in 0..(VGA_WIDTH * VGA_HEIGHT) {
-        unsafe {
-            *vga.add(i * 2) = b' ';
-            *vga.add(i * 2 + 1) = color;
+struct CrisnetOS {
+    cursor_x: u8,
+    cursor_y: u8,
+    vga_memory: *mut u8,
+}
+
+impl CrisnetOS {
+    const VGA_WIDTH: usize = 80;
+    const VGA_HEIGHT: usize = 25;
+
+    fn new() -> CrisnetOS {
+        CrisnetOS {
+            cursor_x: 0,
+            cursor_y: 0,
+            vga_memory: 0xb8000 as *mut u8,
         }
     }
-}
 
-fn advance_cursor(cursor_x: &mut u8, cursor_y: &mut u8) {
-    *cursor_x += 1;
-    if *cursor_x >= VGA_WIDTH as u8 {
-        *cursor_x = 0;
-        *cursor_y += 1;
-    }
-}
-
-fn jump_line(cursor_x: &mut u8, cursor_y: &mut u8) {
-    *cursor_x = 0;
-    if *cursor_y >= VGA_HEIGHT as u8 {
-        *cursor_y = VGA_HEIGHT as u8 - 1;
-    }
-    *cursor_y += 1;
-}
-
-fn print_char(c: char, cursor_x: &mut u8, cursor_y: &mut u8) {
-    let vga = VGA_ADDRESS as *mut u8;
-
-    if c == '\n' {
-        jump_line(cursor_x, cursor_y);
-    } else {
-        let index = *cursor_y as usize * VGA_WIDTH + *cursor_x as usize;
-        unsafe {
-            *vga.add(index * 2) = c as u8;
-            *vga.add(index * 2 + 1) = (BLUE << 4) | WHITE;
-            advance_cursor(cursor_x, cursor_y);
+    fn clear_screen(&self, color: u8) {
+        for i in 0..(Self::VGA_WIDTH * Self::VGA_HEIGHT) {
+            unsafe {
+                *self.vga_memory.add(i * 2) = b' ';
+                *self.vga_memory.add(i * 2 + 1) = color;
+            }
         }
     }
-}
 
-fn print(content: &[u8], cursor_x: &mut u8, cursor_y: &mut u8) {
-    for &byte in content {
-        print_char(byte as char, cursor_x, cursor_y);
+    fn advance_cursor(&mut self) {
+        self.cursor_x += 1;
+        if self.cursor_x >= Self::VGA_WIDTH as u8 {
+            self.cursor_x = 0;
+            self.cursor_y += 1;
+        }
     }
-}
 
-fn println(content: &[u8], cursor_x: &mut u8, cursor_y: &mut u8) {
-    print(content, cursor_x, cursor_y);
-    jump_line(cursor_x, cursor_y);
+    fn jump_line(&mut self) {
+        self.cursor_x = 0;
+        if self.cursor_y >= Self::VGA_HEIGHT as u8 {
+            self.cursor_y = Self::VGA_HEIGHT as u8 - 1;
+        }
+        self.cursor_y += 1;
+    }
+
+    fn print_char(&mut self, c: char) {
+        if c == '\n' {
+            self.jump_line();
+        } else {
+            let index = self.cursor_y as usize * Self::VGA_WIDTH + self.cursor_x as usize;
+            unsafe {
+                *self.vga_memory.add(index * 2) = c as u8;
+                *self.vga_memory.add(index * 2 + 1) =
+                    ((Color::BLUE as u8) << 4) | Color::WHITE as u8;
+                self.advance_cursor();
+            }
+        }
+    }
+
+    fn print(&mut self, content: &[u8]) {
+        for &byte in content {
+            self.print_char(byte as char);
+        }
+    }
+
+    fn println(&mut self, content: &[u8]) {
+        self.print(content);
+        self.jump_line();
+    }
 }
 
 #[no_mangle]
 pub extern "C" fn crisnet_os_main() -> ! {
-    let mut cursor_x: u8 = 0;
-    let mut cursor_y: u8 = 0;
+    let mut crisnet_os = CrisnetOS::new();
 
-    clear_screen((BLUE << 4) | BLUE);
+    crisnet_os.clear_screen(((Color::BLUE as u8) << 4) | Color::BLUE as u8);
 
-    println(b"   ____      _                _       ___  ____", &mut cursor_x, &mut cursor_y);
-    println(b"  / ___|_ __(_)___ _ __   ___| |_    / _ \\/ ___|", &mut cursor_x, &mut cursor_y);
-    println(b" | |   | '__| / __| '_ \\ / _ \\ __|  | | | \\___ \\", &mut cursor_x, &mut cursor_y);
-    println(b" | |___| |  | \\__ \\ | | |  __/ |_   | |_| |___) |", &mut cursor_x, &mut cursor_y);
-    println(b"  \\____|_|  |_|___/_| |_|\\___|\\__|   \\___/|____/", &mut cursor_x, &mut cursor_y);
+    crisnet_os.println(b"   ____      _                _       ___  ____");
+    crisnet_os.println(b"  / ___|_ __(_)___ _ __   ___| |_    / _ \\/ ___|");
+    crisnet_os.println(b" | |   | '__| / __| '_ \\ / _ \\ __|  | | | \\___ \\");
+    crisnet_os.println(b" | |___| |  | \\__ \\ | | |  __/ |_   | |_| |___) |");
+    crisnet_os.println(b"  \\____|_|  |_|___/_| |_|\\___|\\__|   \\___/|____/");
 
-    println(b"", &mut cursor_x, &mut cursor_y);
-    println(b" +-----------------------------+",&mut cursor_x, &mut cursor_y);
-    println(b" +    Crisnet OS Version 0.2   + ",&mut cursor_x, &mut cursor_y);
-    println(b" + ----------------------------+",&mut cursor_x, &mut cursor_y);
-    println(b" +     By Crisnet Inc @ 2026   +", &mut cursor_x, &mut cursor_y);
-    println(b" +-----------------------------+\n",&mut cursor_x, &mut cursor_y);
+    crisnet_os.println(b" ");
+    crisnet_os.println(b" +-----------------------------+");
+    crisnet_os.println(b" +    Crisnet OS Version 0.2   + ");
+    crisnet_os.println(b" + ----------------------------+");
+    crisnet_os.println(b" +     By Crisnet Inc @ 2026   +");
+    crisnet_os.println(b" +-----------------------------+");
 
-    loop {
-        core::hint::spin_loop();
-    }
+    loop {}
 }
+
